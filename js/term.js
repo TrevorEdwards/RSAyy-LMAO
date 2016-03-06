f6e4ab4de0468489d2757c889dccff338def8853
arklian <emeraldarrow@gmail.com>
2016-03-06 02:27:41 -0500

    worked on solutions

--- js/term.js
+++ js/term.js
@@ -6,6 +6,7 @@ var timerid = null;
 var gid = 0;
 var mid = 0;
 var safeToQuery = true;
+var puzzles = [];
 
 function httpGetAsync(theUrl, callback)
 {
@@ -20,28 +21,28 @@ function httpGetAsync(theUrl, callback)
 
 var safeToQuery;
 //Queries the server every second
-function update(){
+function update(term){
   if(safeToQuery){
     safeToQuery = false;
     httpGetAsync(baseurl + "/gameStatus/"+mid+"/"+gid, function(response){
       var resp = JSON.parse(response);
       if (resp.winner){
         //SOMEONE WON WOW
-        console.log('wow someone won');
+          term.echo('wow someone won');
       } else {
         if(mid < resp.gMapNumber){
           //we're out of date
-          updateMap();
+          updateMap(term);
         }
       }
     });
     safeToQuery = true;
   }
-    timerid = setTimeout(function(){update()},1000);
+    timerid = setTimeout(function(){update(term)},1000);
 }//clearInterval(timerid) to stop updating.
-update();
 
-function updateMap(){
+
+function updateMap(term){
   httpGetAsync(baseurl + "/mapInfo/"+uid, function(response){
     var resp = JSON.parse(response);
     for(var i=0; i<resp.length;i++){
@@ -53,10 +54,16 @@ function updateMap(){
 }
 
 
+function getPuzzleInfo(term, n){
+    httpGetAsync(baseurl + "/puzzleInfo/" + uid + '/' + n, function(response){
+	var obj = JSON.parse(response);
+	puzzles[n] =  obj.prompt;
+	term.echo("Puzzle "+n+ " is: " + puzzles[n]);
+    })
 
 
 
-function joinGame(name){
+function joinGame(term,name){
   var frag = '/joingame/'
   var url = baseurl.concat(frag).concat(name);
   httpGetAsync(url,function(callback){
@@ -64,10 +71,12 @@ function joinGame(name){
       term.echo("That name is already taken. Choose another.")
     }
     else{
+	term.echo("Welcome, %s.", name);
 	var obj = JSON.parse(callback);
 	uid = obj.uid;
 	gamestate = 1;
 	ring = 0;
+	update(term);
     }
   }
 )}
@@ -81,12 +90,37 @@ jQuery(function($, undefined) {
       command = command.trim();
 
       if(gamestate == 0){
-        joinGame(command);
+        joinGame(term , command);
       }
       else{
-        switch(command){
+	  var first = command.split[' '][0];
+	  var rest = command.substr(first.length + 1);
+	  
+          switch(first){
           case help: term.echo("Type in the answer to the problem to proceed");
-          break;
+              break;
+	      
+	  case solve: 
+	      var frag = '/solution/';
+	      var url = baseurl.concat(frag).concat(name).concat('/').concat(command);
+	      httpGetAsync(url,function(callback){
+		  var obj = JSON.parse(callback)
+		  if(obj.correct){
+		      term.echo("Good job son, you got it correct.");
+		      moveOn();
+		  }
+		  else{
+		      term.echo("Wrong answer, try again");
+		}
+	      })	
+	      break;
+	  case repeat:
+	      
+	      
+	      
+		
+	
+
 
           default: term.echo("idk");
           break;
