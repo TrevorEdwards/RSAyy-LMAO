3928a2fac716b2d914b3476876aeee91f9f2cd2b
Trevor Edwards <tre23@cornell.edu>
2016-03-06 02:36:42 -0500

    handle merge

--- js/term.js
+++ js/term.js
@@ -31,12 +31,12 @@ function update(term){
       //TRASH TALK @#(*$&@#*($))
       console.log(resp.msg);
       if(resp.msg != lastTrash && resp.msp != undefined){
-          lastTrash = resp.msg;
-        	responsiveVoice.speak(lastTrash);
+        lastTrash = resp.msg;
+        responsiveVoice.speak(lastTrash);
       }
       if (resp.winner){
         //SOMEONE WON WOW
-          term.echo('wow someone won');
+        term.echo('wow someone won');
       } else {
         if(mid < resp.gMapNumber){
           //we're out of date
@@ -46,7 +46,7 @@ function update(term){
     });
     safeToQuery = true;
   }
-    timerid = setTimeout(function(){update(term)},1000);
+  timerid = setTimeout(function(){update(term)},1000);
 }//clearInterval(timerid) to stop updating.
 
 
@@ -69,99 +69,98 @@ function trashTalk(msg){
 
 
 function getPuzzleInfo(term, n){
-    httpGetAsync(baseurl + "/puzzleInfo/" + uid + '/' + n, function(response){
-	var obj = JSON.parse(response);
-	puzzles[n] =  obj.prompt;
-	term.echo("Puzzle "+n+ " is: " + puzzles[n]);
-    })
+  httpGetAsync(baseurl + "/puzzleInfo/" + uid + '/' + n, function(response){
+    var obj = JSON.parse(response);
+    puzzles[n] =  obj.prompt;
+    term.echo("Puzzle "+n+ " is: " + puzzles[n]);
+  })};
 
 
 
-function joinGame(term,name){
-  var frag = '/joingame/'
-  var url = baseurl.concat(frag).concat(name);
-  httpGetAsync(url,function(callback){
-    if(callback == ""){
-      term.echo("That name is already taken. Choose another.")
-    }
-    else{
-	term.echo("Welcome, %s.", name);
-	var obj = JSON.parse(callback);
-	uid = obj.uid;
-	gamestate = 1;
-	ring = 0;
-	update(term);
-    }
-  }
-)}
-
-
-
-jQuery(function($, undefined) {
-  $('#terminal').terminal(function(command, term) {
-
-    if (command !== '') {
-      command = command.trim();
-
-      var firstWord = "NA";
-      var firstBound = command.indexOf(" ");
-      if(firstBound != -1){
-        firstWord = command.substring(0,firstBound);
-      }
-      console.log(firstWord);
-      if(firstWord == "trash"){
-        trashTalk(command.substring(firstBound,command.length));
-        return;
-      }
-
-      if(gamestate == 0){
-        joinGame(term , command);
+  function joinGame(term,name){
+    var frag = '/joingame/'
+    var url = baseurl.concat(frag).concat(name);
+    httpGetAsync(url,function(callback){
+      if(callback == ""){
+        term.echo("That name is already taken. Choose another.")
       }
       else{
-	  var first = command.split[' '][0];
-	  var rest = command.substr(first.length + 1);
-	  
-          switch(first){
-          case help: term.echo("Type in the answer to the problem to proceed");
-              break;
-	      
-	  case solve: 
-	      var frag = '/solution/';
-	      var url = baseurl.concat(frag).concat(name).concat('/').concat(command);
-	      httpGetAsync(url,function(callback){
-		  var obj = JSON.parse(callback)
-		  if(obj.correct){
-		      term.echo("Good job son, you got it correct.");
-		      moveOn();
-		  }
-		  else{
-		      term.echo("Wrong answer, try again");
-		}
-	      })	
-	      break;
-	  case repeat:
-	      
-	      
-	      
-		
-	
-
-
-          default: term.echo("idk");
-          break;
-
-        }
+        term.echo("Welcome, %s.", name);
+        var obj = JSON.parse(callback);
+        uid = obj.uid;
+        gamestate = 1;
+        ring = 0;
+        update(term);
       }
-    } else{
+    }
+  )}
+
+
+
+  jQuery(function($, undefined) {
+    $('#terminal').terminal(function(command, term) {
+
+      if (command !== '') {
+        command = command.trim();
+
+        var firstWord = "NA";
+        var firstBound = command.indexOf(" ");
+        if(firstBound != -1){
+          firstWord = command.substring(0,firstBound);
+        }
+        if(firstWord == "trash"){
+          trashTalk(command.substring(firstBound,command.length));
+          return;
+        }
+
+        if(gamestate == 0){
+          joinGame(term , command);
+        }
+        else{
+          var first = command.split[' '][0];
+          var rest = command.substr(first.length + 1);
+
+          switch(first){
+            case help: term.echo("Type in the answer to the problem to proceed");
+            break;
+
+            case solve:
+            var frag = '/solution/';
+            var url = baseurl.concat(frag).concat(name).concat('/').concat(command);
+            httpGetAsync(url,function(callback){
+              var obj = JSON.parse(callback)
+              if(obj.correct){
+                term.echo("Good job son, you got it correct.");
+                moveOn();
+              }
+              else{
+                term.echo("Wrong answer, try again");
+              }
+            })
+            break;
+            case repeat:
+
+
+
+
+
+
+
+            default: term.echo("idk");
+            break;
+
+          }
+        }
+      } else{
         term.echo("");
       }
     },{
-        greetings: function(callback){
-          const greet = 'Welcome to the game. Please type your name';
-          callback(greet);
-        },
-        name: 'TrevorTerminal', //not necessary
-        height: 200,
-        width: 1001,
-        prompt: ': '});
-      });
+      greetings: function(callback){
+        const greet = 'Welcome to the game. Please type your name';
+        callback(greet);
+      },
+      name: 'TrevorTerminal', //not necessary
+      height: 200,
+      width: 1001,
+      prompt: ': '});
+    });
